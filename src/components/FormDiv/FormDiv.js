3b8a28a2df25dfa3b92631a4ffa7187be30314b3
AchiGel <ar.gelashvili@gmail.com>
2024-02-22 14:11:24 +0400

    change inputs structure

--- src/components/FormDiv/FormDiv.js
+++ src/components/FormDiv/FormDiv.js
@@ -1,41 +1,14 @@
-import { useState } from "react";
+import FormDivInputs from "../FromDivInputs/FormDivInputs";
 import "./formDiv.css";
 
 function FormDiv() {
-  const [firstName, setFirstName] = useState("");
-  const [firstNameError, setFirstNameError] = useState("");
-  const [warning, setWarning] = useState(false);
-
-  function handleTextInput(e) {
-    const value = e.target.value;
-    setFirstName(value);
-
-    if (value.trim() === "") {
-      setFirstNameError("First Name cannot be empty");
-      setWarning(true);
-    } else if (value.length < 2) {
-      setFirstNameError("First name must be at least 2 characters");
-      setWarning(true);
-    } else {
-      setFirstNameError("");
-      setWarning(false);
-    }
-  }
-
   return (
     <div className="form-div">
       <form className="trial-form">
-        <input
-          className={warning ? "trial-form warning-input" : ""}
-          onChange={handleTextInput}
-          type="text"
-          placeholder="First Name"
-          value={firstName}
-        />
-        {firstNameError && <p className="error warning">{firstNameError}</p>}
-        <input type="text" placeholder="First Name" />
-        <input type="email" placeholder="First Name" />
-        <input type="password" placeholder="First Name" />
+        <FormDivInputs type="text" placeholder="First Name" />
+        <FormDivInputs type="text" placeholder="Last Name" />
+        <FormDivInputs type="email" placeholder="Email Address" />
+        <FormDivInputs type="password" placeholder="Password" />
         <button type="submit">CLAIM YOUR FREE TRIAL</button>
       </form>
       <p>
---
+++ src/components/FromDivInputs/FormDivInputs.js
@@ -0,0 +1,5 @@
+function FormDivInputs(props) {
+  return <input type={props.type} placeholder={props.placeholder} />;
+}
+
+export default FormDivInputs;
