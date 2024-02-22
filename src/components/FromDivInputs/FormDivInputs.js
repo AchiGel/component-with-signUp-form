a568ab7c1291ec8690d141e892b412b0be1cfcfb
AchiGel <ar.gelashvili@gmail.com>
2024-02-22 14:30:44 +0400

    Add New Validations

--- src/components/FromDivInputs/FormDivInputs.js
+++ src/components/FromDivInputs/FormDivInputs.js
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 function FormDivInputs(props) {
-  return <input type={props.type} placeholder={props.placeholder} />;
+  const [textContent, setTextContent] = useState("");
+  const [textContentError, setTextContentError] = useState("");
+  const placeholder = props.placeholder;
+  const [warning, setWarning] = useState(false);
+
+  function validateTextContent(e) {
+    const value = e.target.value;
+    setTextContent(value);
+    if (value.trim() === "") {
+      setTextContentError(placeholder + " cannot be empty");
+      setWarning(true);
+    } else {
+      setTextContentError("");
+      setWarning(false);
+    }
+  }
+
+  return (
+    <>
+      <input
+        className={warning ? "warning-input" : ""}
+        onChange={validateTextContent}
+        type={props.type}
+        placeholder={props.placeholder}
+        value={textContent}
+      />
+      {textContentError && <p className="warning">{textContentError}</p>}
+    </>
+  );
 }
 
 export default FormDivInputs;
